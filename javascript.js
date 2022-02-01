7e644c3b34459f176229af85ccb987c9dd673a23
plaxidia <plaxidiahove@gmail.com>
2022-02-01 14:55:21 +0300

    declaring,assigning variables and printing the message

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+let  num =4 ;
+console.log(num);
+
+let usernname = "Maka" ;
+console.log(usernname);
+num =60;
+console.log(num);
+
+ usernname = "Maka's Account  " ;
+console.log(usernname);
