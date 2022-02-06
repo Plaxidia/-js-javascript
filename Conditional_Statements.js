c644ad278642a48f3061b0ff4a8ce8a7f0d5c307
plaxidia <plaxidiahove@gmail.com>
2022-02-06 19:17:43 +0300

    Added Boolean conversion in if statement

--- Conditional_Statements.js
+++ Conditional_Statements.js
@@ -12,3 +12,21 @@ console.log( 'You are right!' );
 console.log( "You're so smart!" );
   
 }
+//Boolean conversion
+//The if (…) statement evaluates the expression in its parentheses and converts the result to a boolean.
+//A number 0, an empty string "", null, undefined, and NaN all become false. Because of that they are called “falsy” values.
+//Other values become true, so they are called “truthy”.
+
+if (0) { // 0 is falsy
+    //...
+  }
+  
+  if (1) { // 1 is truthy
+    //...
+  }
+//We can also pass a pre-evaluated boolean value to if, like this:
+  let cond = (year == 2015); // equality evaluates to true or false
+
+if (cond) {
+  //...
+}
