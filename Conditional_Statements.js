e6fa2ed7399862c48a1ef297fb31d9dcbb1f6d6c
plaxidia <plaxidiahove@gmail.com>
2022-02-06 19:13:38 +0300

    Added if statement in js

---
+++ Conditional_Statements.js
@@ -0,0 +1,14 @@
+//Conditional branching: if, '?'
+
+
+//The “if” statement
+//The if(...) statement evaluates a condition in parentheses and, if the result is true, executes a block of code.
+
+let year = prompt('In which year was ECMAScript-2021 specification published?', '');
+
+if (year == 2021)
+{
+console.log( 'You are right!' );
+console.log( "You're so smart!" );
+  
+}
