a8619507d8e6ec5ed34ab4865f970eb230ce2b7f
plaxidia <plaxidiahove@gmail.com>
2022-02-08 12:50:44 +0300

    example on how to use few template literal in js.

--- Template_Literal.js
+++ Template_Literal.js
@@ -1,1 +1,15 @@
-//
+//Template literal in javascript
+
+let num1 =3;
+let num2 =5;
+let result = num1 +num2 ;
+
+//console.log("The sum of "+ num1 + " and " + num2 + " is equal to :" + result );
+
+//Example on how to use Template literal 
+
+console.log(` The Sum of ${num1} and ${num2} is equal to :${result}`);
+
+//another example
+
+console.log(" My name is \n Plaxidia Hove ");
