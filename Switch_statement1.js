4954ad895161ca2eb21aa6066a87d53b7e13d688
plaxidia <plaxidiahove@gmail.com>
2022-02-08 12:29:50 +0300

    Added a switch block

---
+++ Switch_statement1.js
@@ -0,0 +1,36 @@
+//Mon -7am
+//Tue-Thur -4am
+//Friday -9am
+//Sat-Sun-8am
+
+let day =" Monday";
+
+
+switch(day) {
+    case "Monday ":
+        console.log("7am");
+        break;
+    case "Tuesday":    
+        console.log("4 am");
+        break;
+    case " Wednesday":
+        console.log(" 4 am");
+        break;
+    case " Thursday ":
+        console.log("4 am ");
+        break;
+    case " Friday":
+        console.log("9 am");
+        break;
+    case "Saturday":
+        console.log("8 am");
+        break;
+    case "Sunday":
+        console.log("8 am");
+        break;
+        
+}
+break;
+
+console.log("Week day does not exist");
+    
