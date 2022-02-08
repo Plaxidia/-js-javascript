c326e20da1691802576b676374e155285d0ed555
plaxidia <plaxidiahove@gmail.com>
2022-02-08 13:11:23 +0300

    Added do while loop with an example.

--- Loops-1.js
+++ Loops-1.js
@@ -26,3 +26,21 @@ while (i) { // when i becomes 0, the condition becomes falsy, and the loop stops
 
 i = 3;
 while (i) alert(i--);
+
+//The “do…while” loop
+
+//The condition check can be moved below the loop body using the do..while syntax:
+
+do {
+    // loop body
+  } while (condition);
+
+  //The loop will first execute the body, then check the condition, and, while it’s truthy, execute it again and again.
+
+  //Example
+
+i = 0;
+do {
+  console.log( i );
+  i++;
+} while (i < 3);
