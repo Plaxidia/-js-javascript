8dcc3812ffcf82b995248dea76c60321fdcd580b
plaxidia <plaxidiahove@gmail.com>
2022-02-08 13:07:27 +0300

    while loop

---
+++ Loops-1.js
@@ -0,0 +1,28 @@
+//while loop
+while (condition) {
+    // code
+    // so-called "loop body"
+  }
+
+  //For instance, the loop below outputs i while i < 3:
+
+  let i = 0;
+  while (i < 3) { // shows 0, then 1, then 2
+   console.log( i );
+    i++;
+  } 
+
+  //A single execution of the loop body is called an iteration. The loop in the example above makes three iterations.
+
+
+  //For instance, a shorter way to write while (i != 0) is while (i):
+ i = 3;
+while (i) { // when i becomes 0, the condition becomes falsy, and the loop stops
+  console.log( i );
+  i--;
+}
+
+//Curly braces are not required for a single-line body
+
+i = 3;
+while (i) alert(i--);
