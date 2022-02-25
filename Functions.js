5b28a7660b26d89f669f9d51b1e718066d12e7c6
plaxidia <plaxidiahove@gmail.com>
2022-02-25 23:05:15 +0300

    local variables in function

--- Functions.js
+++ Functions.js
@@ -1,8 +1,25 @@
 // functions 
+//function keyword goes first then goes the name of the function
 function greet ()
 {
-    console.log(" Hello ,It's Plaxidia");
+    console.log(" Hello ,It's Plaxidia");//body
 }//function defination
 
-greet(); //fuction calling
+greet(); //function calling,it executes the code of the fuction
 
+//local variables
+//a variable declared inside a function is only visible inside that function
+
+//example
+    function showMessage() {
+    let message = "Hello, I'm Plaxidia!"; // local variable
+  
+  console.log( message );
+  }
+  
+  showMessage(); // Hello, I'm Plaxidia!
+  
+  console.log( message ); // <-- Error! The variable is local to the function
+
+
+  
