e39f9b2e43f4ef8b7e1bd0d6a9729cd88d15e6eb
plaxidia <plaxidiahove@gmail.com>
2022-02-25 23:17:21 +0300

    Outer variables and function

--- Functions.js
+++ Functions.js
@@ -21,5 +21,48 @@ greet(); //function calling,it executes the code of the fuction
   
   console.log( message ); // <-- Error! The variable is local to the function
 
+//outer variables
+//function can access an outer as well 
 
-  
+  let userName = 'Hove';
+
+  function showMessage()
+  {
+  let message = 'Hello, ' + userName;
+  console.log(message);
+  }
+
+   showMessage(); // Hello, Hove
+  //The function has full access to the outer variable. It can modify it as well.
+  //example
+
+  let userName1 = 'Hove';
+
+  function showMessage1() {
+  userName = "Chiko"; // (1) changed the outer variable
+
+  let showMessage1 = 'Hello, ' + userName1;
+  console.log(showMessage1);
+}
+
+console.log( userName1 ); // Hove ,before the function call
+
+showMessage1();
+
+console.log( userName1 ); // chiko, the value was modified by the function
+
+//The outer variable is only used if there’s no local one.
+
+let userName2 = 'John';
+
+function showMessage2() {
+  let userName = "Bob"; // declare a local variable
+
+  let message2 = 'Hello, ' + userName2; // Bob
+ console.log(message2);
+}
+
+// the function will create and use its own userName
+showMessage2();
+
+console.log( userName2 ); // John, unchanged, the function did not access the outer variable
