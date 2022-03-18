a9c9967165f9aae9165df8d8d3cc977c1d5f759e
plaxidia <plaxidiahove@gmail.com>
2022-03-18 14:41:35 +0300

    edited

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 let user = new Object(); // "object constructor" syntax
-let user = {};  // "object literal" syntax
+user = {};  // "object literal" syntax
 
 //Literals and properties
 //We can immediately put some properties into {...} as “key: value” pairs:
@@ -94,9 +94,9 @@ delete user.age;
 
 //We can use square brackets in an object literal, when creating an object. That’s called computed properties.
 
-let fruit = prompt("Which fruit to buy?", "apple");
+fruit = prompt("Which fruit to buy?", "apple");
 
-let bag = {
+ bag = {
   [fruit]: 5, // the name of the property is taken from the variable fruit
 };
 
