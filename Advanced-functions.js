6dd9c5880ff5f03ee3ec23eece6d9359258152b8
plaxidia <plaxidiahove@gmail.com>
2022-03-25 22:37:08 +0300

    recursive stractures

--- Advanced-functions.js
+++ Advanced-functions.js
@@ -146,4 +146,26 @@ function pow(x, n) {
   }
   
   console.log(sumSalaries(company)); // 770054321
-  
+  //Recursive structures
+  /*
+  A recursive (recursively-defined) data structure is a structure that replicates itself in parts.
+
+We’ve just seen it in the example of a company structure above.
+
+A company department is:
+
+Either an array of people.
+Or an object with departments.
+For web-developers there are much better-known examples: HTML and XML documents.
+
+In the HTML document, an HTML-tag may contain a list of:
+
+Text pieces.
+HTML-comments.
+Other HTML-tags (that in turn may contain text pieces/comments or other tags etc).
+That’s once again a recursive definition.
+
+For better understanding, we’ll cover one more recursive structure named “Linked list” that might be a better alternative for arrays in some cases.
+*/
+
+
