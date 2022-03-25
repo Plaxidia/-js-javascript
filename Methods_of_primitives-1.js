91fd15f4709f14fd2f5ef477b1c70a42651e4750
plaxidia <plaxidiahove@gmail.com>
2022-03-25 18:57:43 +0300

    added  primitive as an object

--- Methods_of_primitives-1.js
+++ Methods_of_primitives-1.js
@@ -17,3 +17,37 @@ let john = {
   };
   
   john.sayHi(); // Hi buddy!
+
+  //A primitive as an object
+  //Primitives must be as fast and lightweight as possible.
+  //There are many things one would want to do with a primitive like a string or a number. It would be great to access them using methods.
+//For instance, there exists a string method str.toUpperCase() that returns a capitalized str.
+//example
+let str = "Hello";
+
+console.log( str.toUpperCase() ); // HELLO
+
+//A number has methods of its own, for instance, toFixed(n) rounds the number to the given precision:
+let n = 1.23456;
+
+console.log( n.toFixed(2) ); // 1.23
+
+//In JavaScript, that’s also possible for historical reasons, but highly unrecommended. Things will go crazy in several places.
+console.log( typeof 0 ); // "number"
+
+console.log( typeof new Number(0) ); // "object"!
+//Objects are always truthy in if, so here the alert will show up:
+
+let zero = new Number(0);
+
+if (zero) { // zero is true, because it's an object
+  console.log( "zero is truthy!?!" );
+}
+
+//On the other hand, using the same functions String/Number/Boolean without new is a totally sane and useful thing. They convert a value to the corresponding type: to a string, a number, or a boolean (primitive).
+
+
+let num = Number("123"); // convert a string to number
+
+//null/undefined have no methods
+console.log(null.test); // error
